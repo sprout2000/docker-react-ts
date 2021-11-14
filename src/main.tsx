48d1fb695a04076e094f5946c836f40e0f877ae1
sprout2000 <wassabie64@gmail.com>
2021-11-14 12:45:38 +0900

    Updated App.tsx and main.tsx.

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+import { App } from './App';
+import './index.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
